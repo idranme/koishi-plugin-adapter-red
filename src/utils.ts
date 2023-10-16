cba922cc8535d03976ff505beefcde3086620e83
idanran <idanran@outlook.com>
2023-10-17 01:43:00 +0800

    1.7.5

--- src/utils.ts
+++ src/utils.ts
@@ -106,7 +106,9 @@ export async function decodeMessage(
                 // https://gchat.qpic.cn/url
                 // https://pic.ugcimg.cn/md5
                 const url = v.picElement.originImageUrl
-                if (url.includes('rkey')) {
+                if (!url) {
+                    result.push(h.image(`https://gchat.qpic.cn/gchatpic_new/0/0-0-${v.picElement.md5HexStr.toUpperCase()}/0`))
+                } else if (url.includes('rkey')) {
                     const file = await getFile(bot, data, v.elementId)
                     const extension = v.picElement.sourcePath.split('.').at(-1)
                     result.push(h.image(file.data, mimes[extension] ?? 'application/octet-stream'))
