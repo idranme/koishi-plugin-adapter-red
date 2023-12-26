431d8f425e462c10d56eff3b6ffe473180d4161e
idanran <idanran@outlook.com>
2023-12-27 02:20:23 +0800

    2.7.2

--- src/silk.ts
+++ src/silk.ts
@@ -1,10 +1,16 @@
 /**
  * Forked from https://github.com/xfdown/xiaofei-plugin/blob/master/model/silk_worker/index.cjs
- * Original license: https://github.com/xfdown/xiaofei-plugin/blob/master/LICENSE
+ * Its license: https://github.com/xfdown/xiaofei-plugin/blob/master/LICENSE
  */
 import { encode, decode, getDuration, encodeResult, decodeResult } from 'silk-wasm'
 import { isMainThread, parentPort, Worker, MessageChannel } from 'node:worker_threads'
 import { Dict } from 'koishi'
+import { cpus } from 'node:os'
+
+interface WorkerInstance {
+    worker: Worker
+    busy: boolean
+}
 
 if (!isMainThread && parentPort) {
     parentPort.addListener('message', (val) => {
@@ -12,19 +18,31 @@ if (!isMainThread && parentPort) {
         const port: MessagePort = val.port
         switch (data?.type) {
             case "encode":
-                encode(data.input, data.sampleRate).then(ret => {
-                    port.postMessage(ret)
-                    port.close()
-                })
+                encode(data.input, data.sampleRate)
+                    .then(ret => {
+                        port.postMessage(ret)
+                    }).catch(err => {
+                        port.postMessage(err)
+                    }).finally(() => {
+                        port.close()
+                    })
                 break
             case "decode":
                 decode(data.input, data.sampleRate).then(ret => {
                     port.postMessage(ret)
+                }).catch(err => {
+                    port.postMessage(err)
+                }).finally(() => {
                     port.close()
                 })
                 break
             case "getDuration":
-                const ret = getDuration(data.silk, data.frameMs)
+                let ret: number
+                try {
+                    ret = getDuration(data.silk, data.frameMs)
+                } catch (err) {
+                    ret = err
+                }
                 port.postMessage(ret)
                 port.close()
                 break
@@ -35,18 +53,51 @@ if (!isMainThread && parentPort) {
     })
 }
 
-let worker: Worker
+const workers: WorkerInstance[] = []
+const numCPUs = cpus().length
+let availability = 0
 
 function postMessage<T extends any>(data: Dict): Promise<T> {
-    worker ||= new Worker(__filename)
+    let indexing = 0
+    if (workers.length === 0) {
+        workers.push({
+            worker: new Worker(__filename),
+            busy: false
+        })
+        availability++
+    } else {
+        let found = false
+        for (const [index, value] of workers.entries()) {
+            if (value?.busy === false) {
+                indexing = index
+                found = true
+                break
+            }
+        }
+        if (!found) {
+            const len = workers.push({
+                worker: new Worker(__filename),
+                busy: false
+            })
+            availability++
+            indexing = len - 1
+        }
+    }
+    workers[indexing].busy = true
     const subChannel = new MessageChannel()
     const port = subChannel.port2
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         port.once('message', (ret) => {
             port.close()
-            resolve(ret)
+            workers[indexing].busy = false
+            if (availability > numCPUs - 1) {
+                workers[indexing].worker.terminate()
+                workers[indexing] = undefined
+                availability--
+            }
+            ret instanceof Error ? reject(ret) : resolve(ret)
         })
-        worker.postMessage({ port: subChannel.port1, data: data }, [subChannel.port1])
+        workers[indexing].worker.postMessage({ port: subChannel.port1, data: data }, [subChannel.port1])
     })
 }
 
