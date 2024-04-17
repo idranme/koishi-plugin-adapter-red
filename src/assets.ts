728f2663346d882e3fbea3e472163faa77d51395
idanran <idanran@outlook.com>
2024-04-17 23:53:30 +0800

    3.1.2

--- src/assets.ts
+++ src/assets.ts
@@ -29,56 +29,61 @@ export class RedAssets<C extends Context = Context> {
         if (this.config.selfUrl) {
             return trimSlash(this.config.selfUrl)
         }
-        let { host, port } = this.bot.ctx.server
-        if (['0.0.0.0', '::'].includes(host)) host = '127.0.0.1'
-        return this.bot.ctx.server.selfUrl || `http://${host}:${port}`
+        const { server } = this.bot.ctx
+        let host = server?.host ?? '127.0.0.1'
+        if (['0.0.0.0', '::'].includes(host)) {
+            host = '127.0.0.1'
+        }
+        return server?.selfUrl || `http://${host}:${server?.port}`
     }
     private listen() {
-        this.bot.ctx.server.get(this.path, async (ctx) => {
-            ctx.body = '200 OK'
-            ctx.status = 200
-        })
-        this.bot.ctx.server.get(this.path + '/:data', async (ctx, next) => {
-            const data = ctx.params['data']
-            let payload: Dict
-            if (data.endsWith('=')) {
-                payload = JSON.parse(Buffer.from(data, 'base64').toString())
-            } else {
-                payload = JSON.parse(Buffer.from(data, 'base64url').toString())
-            }
-            const mime = payload.mime
-            let file: Quester.Response<ReadableStream>
-            try {
-                file = await this.bot.internal.getFileStream({
-                    msgId: payload.msgId,
-                    chatType: payload.chatType,
-                    peerUid: payload.peerUid,
-                    elementId: payload.elementId,
-                })
-            } catch (err) {
-                if (!Quester.Error.is(err)) {
-                    throw err
+        this.bot.ctx.on('server/ready', () => {
+            this.bot.ctx.server.get(this.path, async (ctx) => {
+                ctx.body = '200 OK'
+                ctx.status = 200
+            })
+            this.bot.ctx.server.get(this.path + '/:data', async (ctx, next) => {
+                const data = ctx.params['data']
+                let payload: Dict
+                if (data.endsWith('=')) {
+                    payload = JSON.parse(Buffer.from(data, 'base64').toString())
+                } else {
+                    payload = JSON.parse(Buffer.from(data, 'base64url').toString())
                 }
-                if (mime.includes('image')) {
-                    try {
-                        file = await this.bot.ctx.http<ReadableStream>(`https://gchat.qpic.cn/gchatpic_new/0/0-0-${payload.md5.toUpperCase()}/0`, {
-                            method: 'GET',
-                            responseType: 'stream'
-                        })
-                    } catch { }
+                const mime = payload.mime
+                let file: Quester.Response<ReadableStream>
+                try {
+                    file = await this.bot.internal.getFileStream({
+                        msgId: payload.msgId,
+                        chatType: payload.chatType,
+                        peerUid: payload.peerUid,
+                        elementId: payload.elementId,
+                    })
+                } catch (err) {
+                    if (!Quester.Error.is(err)) {
+                        throw err
+                    }
+                    if (mime.includes('image')) {
+                        try {
+                            file = await this.bot.ctx.http<ReadableStream>(`https://gchat.qpic.cn/gchatpic_new/0/0-0-${payload.md5.toUpperCase()}/0`, {
+                                method: 'GET',
+                                responseType: 'stream'
+                            })
+                        } catch { }
+                    }
+                    file ||= err.response
                 }
-                file ||= err.response
-            }
 
-            ctx.status = file.status
-            const contentType = file.headers.get('content-type')
-            if (contentType) {
-                ctx.type = contentType
-            } else if (file.status === 200) {
-                ctx.type = mime
-            }
-            ctx.body = Readable.fromWeb(file.data)
-            return next()
+                ctx.status = file.status
+                const contentType = file.headers.get('content-type')
+                if (contentType) {
+                    ctx.type = contentType
+                } else if (file.status === 200) {
+                    ctx.type = mime
+                }
+                ctx.body = Readable.fromWeb(file.data)
+                return next()
+            })
         })
     }
 }
